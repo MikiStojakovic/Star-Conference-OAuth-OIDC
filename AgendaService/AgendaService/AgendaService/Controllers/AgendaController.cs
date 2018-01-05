602465727a89213b75d74f7a899bad10aa22fb00
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2018-01-05 17:06:52 +0100

    Added resource server

---
+++ AgendaService/AgendaService/AgendaService/Controllers/AgendaController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgendaService.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaService.Controllers
+{
+    [Authorize]
+    //[Route("api/[controller]")]
+    [EnableCors("CorsPolicy")]
+    public class AgendaController : Controller
+    {
+        // GET api/values
+        // [HttpGet]
+        // public IEnumerable<string> Get()
+        // {
+        //     return new string[] { "value1", "value2" };
+        // }
+
+        public static List<TodoItem> Todos = TodoItem.CreateList();
+
+        [HttpGetAttribute("api/agenda")]
+        public List<TodoItem> GetTodos()
+        {
+            return Todos;
+        }
+
+        [HttpGet("api/agenda/{title}")]
+        public TodoItem GetTodo(string title)
+        {
+            var todo = Todos.FirstOrDefault(td => td.Title == title);
+            return todo;
+        }
+
+        [HttpDelete("api/agenda/{title}")]
+        public bool DeleteTodo(string title)
+        {
+            var match = Todos.FirstOrDefault(td => td.Title == title);
+            if (match != null)
+                Todos.Remove(match);
+
+            return true;
+        }
+
+
+        [HttpPut("api/agenda")]
+        [HttpPost("api/agenda")]
+        public bool UpdateTodo([FromBody] TodoItem todo)
+        {
+            var existingTodo = Todos.FirstOrDefault(td => td.Title == todo.Title);
+            if (existingTodo != null)
+            {
+                int idx = Todos.IndexOf(existingTodo);
+                Todos[idx] = todo;
+            }
+            else
+                Todos.Insert(0, todo);
+
+            return true;
+        }
+
+        [HttpGet("agenda/status")]
+        public object Status()
+        {
+            return new
+            {
+                OS = System.Runtime.InteropServices.RuntimeInformation.OSDescription,
+                MachineName = System.Environment.MachineName
+            };
+        }
+    }
+}
---
+++ AgendaService/AgendaService/AgendaService/Model/TodoItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaService.Model
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public string Speaker { get; set; }
+        public bool Completed { get; set; }
+        public DateTime Entered { get; set; }
+
+        public static List<TodoItem> CreateList()
+        {
+            return new List<TodoItem> {
+                    new TodoItem {
+                    Title = "Angular 2",
+                    Speaker = "Ivana Tesanovic",
+                    Completed = true,
+                    Entered = DateTime.UtcNow.AddHours(-2),
+                    },
+                    new TodoItem {
+                        Title = "HTTP Caching",
+                        Speaker = "Dejan Ostojic",
+                        Completed = true,
+                        Entered = DateTime.UtcNow.AddHours(-1)
+                    },
+                    new TodoItem {
+                        Title = "Authentification & Authorization",
+                        Speaker = "Miroslav Stojakovic",
+                        Entered = DateTime.UtcNow
+                    },
+                    new TodoItem {
+                        Title = "Get Real (Time) With Websockets",
+                        Speaker = "Milos Davidovic",
+                        Entered = DateTime.UtcNow.AddHours(0.5)
+                    },
+                    new TodoItem {
+                        Title = "Umbraco: Tips & Tricks",
+                        Speaker = "Nenad Maljugic",
+                        Entered = DateTime.UtcNow.AddHours(1.5)
+                    },
+                    new TodoItem {
+                        Title = "MVC 5 Custom Model Binding",
+                        Speaker = "Miroljub Enjakovic",
+                        Entered = DateTime.UtcNow.AddHours(2)
+                    },
+                    new TodoItem {
+                        Title = "Visitor Pattern Explained",
+                        Speaker = "Milan Kosanovic",
+                        Entered = DateTime.UtcNow.AddHours(3)
+                    },
+                    new TodoItem {
+                        Title = "BDD & TDD kroz primere u C#",
+                        Speaker = "Ognjen Stanic",
+                        Entered = DateTime.UtcNow.AddHours(3.5)
+                    }
+                };
+        }
+    }
+}
